2a6ffc557c725b61787c0b7de272fa75bfbf26e0
Justin Ji <jjeui0308@gmail.com>
2019-12-25 09:53:30 +0900

    TLSAppDelegate fixed

--- Source/TLSAppDelegate.swift
+++ Source/TLSAppDelegate.swift
@@ -11,16 +11,6 @@ open class TLSAppDelegate: UIResponder, UIApplicationDelegate {
     
     public var window: UIWindow?
     
-    public var backButtonImage: UIImage? {
-        didSet {
-            guard let image = self.backButtonImage?.stretchableImage(withLeftCapWidth: 0, topCapHeight: 10) else {
-                return
-            }
-            UIBarButtonItem.appearance().title = ""
-            
-        }
-    }
-    
     public func setUpNavigationBar(tintColor: UIColor? = nil,
                                    backgroundColor: UIColor? = nil) {
         
@@ -34,46 +24,5 @@ open class TLSAppDelegate: UIResponder, UIApplicationDelegate {
         window?.makeKeyAndVisible()
         let rootVC = isNavEmbedded ? UINavigationController(rootViewController: rootViewController) : rootViewController
         window?.rootViewController = rootVC
-    }
-    
-    public func notificationConfiguration(application: UIApplication) {
-        
-        if #available(iOS 10.0, *) {
-          UNUserNotificationCenter.current().delegate = self
-
-          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-          UNUserNotificationCenter.current().requestAuthorization(
-            options: authOptions,
-            completionHandler: {_, _ in })
-        } else {
-          let settings: UIUserNotificationSettings =
-          UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-          application.registerUserNotificationSettings(settings)
-        }
-
-        application.registerForRemoteNotifications()
-    }
-    
-}
-
-
-@available(iOS 10, *)
-extension TLSAppDelegate : UNUserNotificationCenterDelegate {
-    
-    public func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                willPresent notification: UNNotification,
-                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        
-//        let userInfo = notification.request.content.userInfo
-        
-        completionHandler([.alert])
-    }
-    
-    public func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                didReceive response: UNNotificationResponse,
-                                withCompletionHandler completionHandler: @escaping () -> Void) {
-//        let userInfo = response.notification.request.content.userInfo
-        
-        completionHandler()
     }
 }
